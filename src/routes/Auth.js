18debfdcf0cdebf4b260764216b56b18468de370
ejrvheowkd <hoao1313@naver.com>
2020-12-03 16:40:41 +0900

    #2.3 Creating Account

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,8 +1,10 @@
 import React, { useCallback, useState } from "react";
+import { authService } from "../fBase";
 
 const Auth = ()=>{
     const [email,setEmail] = useState("");
     const [password,setPassword] = useState("");
+    const [newAccount,setNewAccount] = useState(true);
     const onChange = (event) =>{
         const {target:{name,value}} =event;
         if(name==="email")
@@ -14,15 +16,26 @@ const Auth = ()=>{
             setPassword(value);
         }
     };
-    const onSubmit=(event)=>{
-        event.preventDefault();
+    const onSubmit=async(event)=>{
+        let data;
+        event.preventDefault();//새로고침을 막고 event 사용 안하면 사라짐 정보
+        try{
+        if(newAccount){
+             data =await authService.createUserWithEmailAndPassword(email,password);
+        } else {
+             data = await authService.signInWithEmailAndPassword(email,password);    
+        }
+        console.log(data);
+    } catch(error){
+        console.log(error);
+    }
     };
     return(
     <div>
         <form onSubmit={onSubmit}>
-            <input name ="email" type="text" placeholder="email" required value={email} onChange={onChange}/>
+            <input name ="email" type="email" placeholder="email" required value={email} onChange={onChange}/>
             <input name = "password" type="password" placeholder="Possword" required value={password} onChange={onChange}/>
-            <input type="submit" value="Log in"/>
+            <input type="submit" value={newAccount?"Create Account":"Log In"}/>
         </form>
         <div>
             <button>Continue with Google</button>
