7aea8ea8836e51d395d594c0adb6b3ff448c097d
ejrvheowkd <hoao1313@naver.com>
2020-12-04 00:10:46 +0900

    #2.4 Log In

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,27 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import AppRouter from "./Router";
 import {aushService} from "fBase";
 import { authService } from "../fBase";
 
 function App() {
-  const [isLoggedIn,setIsLoggedIn] = useState(authService.currentUser);
+  const [init,setInit] = useState(false);
+  const [isLoggedIn,setIsLoggedIn] = useState(false);
+  
+  useEffect(()=>{
+    //변화가 있는지 듣는 구간
+    authService.onAuthStateChanged((user)=>{
+      if(user){
+        setIsLoggedIn(true);
+      }
+      else{
+        setIsLoggedIn(false);
+      }
+      setInit(true); 
+    });
+  },[]);
   return (
     <>
-  <AppRouter isLoggedIn={isLoggedIn}/>
+  {init?<AppRouter isLoggedIn={isLoggedIn}/>:"Inittializing..."}
   <footer>&copy;Nwitter{new Date().getFullYear()}</footer>
   </>
   );
